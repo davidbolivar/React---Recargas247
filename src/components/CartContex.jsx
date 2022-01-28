274a5996ebfa8cef3de594377438bb6f0808a432
David L Bolívar H <davidbolivarh@gmail.com>
2022-01-27 22:24:19 -0300

    Se modificó Readme y se corrigió error de contador en ItemCount

--- src/components/CartContex.jsx
+++ src/components/CartContex.jsx
@@ -5,5 +5,5 @@ export const CartContext = createContext();
 export function CartProvider({ children }) {
 	const [carrito, setCarrito] = useState(["Producto1", "Producto2"]);
 
-	return <CartContext.Provider value={{ carrito, setCarrito }}>{children};</CartContext.Provider>;
+	return <CartContext.Provider value={{ carrito, setCarrito }}>{children}</CartContext.Provider>;
 }
--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -5,24 +5,30 @@ import Loader from "./Loader";
 
 const ItemDetailContainer = () => {
 	const [itemDetail, setItemDetail] = useState({});
+	const [loading, setLoading] = useState(true);
 	const { service_id } = useParams();
+	let ready = false;
+
+	const getItem = new Promise((resolve, reject) => {
+		fetch("/api/products.json")
+			.then((response) => response.json())
+			.then((res) => {
+				console.log(res);
+				resolve(res);
+			});
+	});
 
 	useEffect(() => {
-		const getItem = new Promise((resolve, reject) => {
-			fetch("/api/products.json")
-				.then((response) => response.json())
-				.then((res) => {
-					console.log(res);
-					resolve(res);
-				});
-		});
-
+		setLoading(true);
 		getItem
 			.then((res) => {
 				console.log(res);
+				console.log(getItem);
+
 				// Se realiza un find para obtener solo 1 item y guardarlo en el estado itemDetail;
 				let details = res.find((item) => item.service_id === service_id && item.active);
 				details ? setItemDetail([details]) : setItemDetail([]);
+				setLoading(false);
 			})
 			.catch((err) => {
 				console.error(err);
@@ -32,7 +38,7 @@ const ItemDetailContainer = () => {
 
 	return (
 		<>
-			{itemDetail.length ? (
+			{!loading ? (
 				itemDetail.map((item) => {
 					return <ItemDetail props={item} />;
 				})
